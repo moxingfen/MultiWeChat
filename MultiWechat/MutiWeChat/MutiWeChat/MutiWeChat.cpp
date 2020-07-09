e515814938e6d3829e12e70965649564c9cdc17a
moxingfen <449759@qq.com>
2020-07-09 12:33:16 +0800

    提交测试

---
+++ MultiWechat/MutiWeChat/MutiWeChat/MutiWeChat.cpp
@@ -0,0 +1,32 @@
+﻿// MutiWeChat.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <AclAPI.h>
+int main()
+{
+	// TODO: 在此处添加实现代码.
+	HANDLE hMutex = CreateMutexW(NULL, FALSE, L"_WeChat_App_Instance_Identity_Mutex_Name");
+	SID_IDENTIFIER_AUTHORITY SIDAuthWorld = SECURITY_WORLD_SID_AUTHORITY; //当前运行权限
+	PSID pEveryoneSID = NULL;
+	char szBuffer[4096];
+	PACL pAcl = (PACL)szBuffer;
+	AllocateAndInitializeSid(&SIDAuthWorld,1,SECURITY_WORLD_RID,0, 0, 0, 0, 0, 0, 0,&pEveryoneSID);
+	InitializeAcl(pAcl, sizeof(szBuffer), ACL_REVISION);
+	AddAccessDeniedAce(pAcl, ACL_REVISION, MUTEX_ALL_ACCESS, pEveryoneSID);
+	SetSecurityInfo(hMutex, SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL, pAcl, NULL);
+
+	system("pause");
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
